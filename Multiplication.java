4390abe5c38fe8f1bd033546228d0e8bae503051
Francis Patawah <phrancisp0@gmail.com>
2023-11-05 23:42:54 -0500

    Multiplication table 6kyu

---
+++ Multiplication.java
@@ -0,0 +1,27 @@
+public class Multiplication{
+
+      public static int[][] multiplicationTable(int n) {
+        int[][] array = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                array[i][j] = (i + 1) * (j + 1);
+            }
+        }
+        
+        return array;
+    }
+  
+}
+
+/*
+Your task, is to create N×N multiplication table, of size provided in parameter.
+
+For example, when given size is 3:
+
+1 2 3
+2 4 6
+3 6 9
+For the given example, the return value should be:
+
+[[1,2,3],[2,4,6],[3,6,9]]
+*/
